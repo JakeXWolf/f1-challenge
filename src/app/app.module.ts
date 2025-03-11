108382a4db34985609cf75ba3c94c709c446dddd
JakeXWolf <jakewolf32@gmail.com>
2025-03-10 22:55:10 -0400

    upgrade to angular 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { DragDropModule } from '@angular/cdk/drag-drop';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
@@ -16,27 +16,19 @@ import { HomeComponent } from './core/components/home/home.component';
 import { GenerateLineupComponent } from './core/components/generate-lineup/generate-lineup.component';
 import { DriverChampStandingsComponent } from './core/components/driver-champ-standings/driver-champ-standings.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    DriverChampStandingsComponent,
-    GenerateLineupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-
-    MatTableModule,
-    MatButtonModule,
-    DragDropModule
-  ],
-  providers: [
-    provideAnimationsAsync(),
-
-    DataService
-  ],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HomeComponent,
+        DriverChampStandingsComponent,
+        GenerateLineupComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        MatTableModule,
+        MatButtonModule,
+        DragDropModule], providers: [
+        provideAnimationsAsync(),
+        DataService,
+        provideHttpClient(withInterceptorsFromDi())
+    ] })
 export class AppModule { }
